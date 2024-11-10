d76a8dbb71cbad6bb319d36e5d3de184da9a1dda
yomancode <yordiasfaw9@gmail.com>
2024-11-10 23:33:08 +0300

    views and templates

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,16 @@
-# relationship_app/urls.py
+# relationship_app/views.py
 
-from django.urls import path
-from .views import LibraryDetailView
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
-urlpatterns = [
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
+# Function-based view to list all books and their authors
+def list_books(request):
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view to display details for a specific library
+class LibraryDetailView(DetailView):
+    model = Library  # The model to retrieve data from
+    template_name = 'relationship_app/library_detail.html'  # Template for displaying library details
+    context_object_name = 'library'  # The context name that will hold the library data in the template

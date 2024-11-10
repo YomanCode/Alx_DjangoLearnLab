1ea6e5cc4259940fa049b5b7e3179a1a32935cce
yomancode <yordiasfaw9@gmail.com>
2024-11-10 22:54:28 +0300

    ORM

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -9,28 +9,36 @@ django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author
+# 1. Query all books by a specific author using .filter()
 def query_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = author.books.all()  # Using related_name 'books'
-    print(f"Books by {author.name}:")
-    for book in books:
-        print(f"- {book.title}")
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)  # Using .filter() method
+        print(f"Books by {author.name} (using .filter()):")
+        for book in books:
+            print(f"- {book.title}")
+    except Author.DoesNotExist:
+        print(f"No author found with the name '{author_name}'.")
 
 # 2. List all books in a specific library
 def query_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()  # Using related_name 'books'
-    print(f"Books in the library '{library.name}':")
-    for book in books:
-        print(f"- {book.title}")
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()  # Using related_name 'books'
+        print(f"Books in the library '{library.name}':")
+        for book in books:
+            print(f"- {book.title}")
+    except Library.DoesNotExist:
+        print(f"No library found with the name '{library_name}'.")
 
 # 3. Retrieve the librarian for a specific library
 def query_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
     try:
+        library = Library.objects.get(name=library_name)
         librarian = Librarian.objects.get(library=library)
         print(f"The librarian for '{library.name}' is {librarian.name}.")
+    except Library.DoesNotExist:
+        print(f"No library found with the name '{library_name}'.")
     except Librarian.DoesNotExist:
         print(f"No librarian found for the library '{library.name}'.")
 

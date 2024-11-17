a3b96a130ad8b16349310ac27f2d3e3fbe8779ae
yomancode <yordiasfaw9@gmail.com>
2024-11-17 15:43:49 +0300

    models and views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,32 +1,24 @@
 from django.db import models
 
-# Author Model
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
-# Book Model
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
-
-# Library Model
-class Library(models.Model):
-    name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
-
-# Librarian Model
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
-
-    def __str__(self):
-        return self.name

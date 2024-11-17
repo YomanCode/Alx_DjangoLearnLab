12a318e97d45edfa86ae3a26708ad069a6dda453
yomancode <yordiasfaw9@gmail.com>
2024-11-17 16:05:17 +0300

    templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,28 +1,43 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Library
-from .models import Book
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
-def list_books(request):
-    """
-    Function-based view to display all books
-    """
-    books = Book.objects.all().select_related('author')
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    success_url = reverse_lazy('book-list')
+    redirect_authenticated_user = True
 
-class LibraryDetailView(DetailView):
-    """
-    Class-based view to display library details
-    """
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+    next_page = reverse_lazy('login')
 
-    def get_queryset(self):
-        """
-        Optimize query by prefetching related books and their authors
-        """
-        return Library.objects.prefetch_related(
-            'books', 
-            'books__author'
-        )
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book-list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# urls.py
+from django.urls import path
+from .views import list_books, LibraryDetailView, CustomLoginView, CustomLogoutView, register
+
+urlpatterns = [
+    path('books/', list_books, name='book-list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+]
+
+# settings.py additions (add these to your project's settings.py)
+LOGIN_REDIRECT_URL = 'book-list'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'

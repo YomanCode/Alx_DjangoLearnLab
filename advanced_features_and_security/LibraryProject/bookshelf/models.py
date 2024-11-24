ef3eb9ec082bf103d4e8d228ee7694b0415a6bcf
yomancode <yordiasfaw9@gmail.com>
2024-11-24 10:24:49 +0300

    permission

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,15 +1,22 @@
+# models.py
 from django.db import models
 
-class Article(models.Model):  # Example model
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Book(models.Model):
+    title = models.CharField(max_length=200)  # Book title
+    author = models.CharField(max_length=100)  # Author name
+    published_date = models.DateField()  # Date of publication
+    isbn = models.CharField(max_length=13, unique=True)  # ISBN number
+    summary = models.TextField(blank=True, null=True)  # Optional summary of the book
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set on creation
+    updated_at = models.DateTimeField(auto_now=True)  # Automatically updated on save
 
     class Meta:
         permissions = [
-            ("can_view", "Can view articles"),
-            ("can_create", "Can create articles"),
-            ("can_edit", "Can edit articles"),
-            ("can_delete", "Can delete articles"),
+            ("can_view", "Can view books"),
+            ("can_create", "Can create books"),
+            ("can_edit", "Can edit books"),
+            ("can_delete", "Can delete books"),
         ]
+
+    def __str__(self):
+        return self.title  # String representation of the model

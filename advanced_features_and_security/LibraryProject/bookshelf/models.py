74fbef0fa69ed96bc78001215b111903fb19af43
yomancode <yordiasfaw9@gmail.com>
2024-11-24 10:23:04 +0300

    permission

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,31 +1,15 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Custom User Manager
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field is required")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+class Article(models.Model):  # Example model
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if not extra_fields.get('is_staff'):
-            raise ValueError("Superuser must have is_staff=True.")
-        if not extra_fields.get('is_superuser'):
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(username, email, password, **extra_fields)
-
-# Custom User Model
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)  # Optional date of birth
-    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True)  # Optional profile photo
-
-    objects = CustomUserManager()  # Assign custom user manager
+    class Meta:
+        permissions = [
+            ("can_view", "Can view articles"),
+            ("can_create", "Can create articles"),
+            ("can_edit", "Can edit articles"),
+            ("can_delete", "Can delete articles"),
+        ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponseForbidden
+from myapp.models import Article
 
-# Create your views here.
+@permission_required('myapp.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'myapp/article_list.html', {'articles': articles})
+
+@permission_required('myapp.can_create', raise_exception=True)
+def create_article(request):
+    if request.method == "POST":
+        # Process form submission to create an article
+        pass
+    return render(request, 'myapp/create_article.html')
+
+@permission_required('myapp.can_edit', raise_exception=True)
+def edit_article(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        # Process form submission to edit the article
+        pass
+    return render(request, 'myapp/edit_article.html', {'article': article})
+
+@permission_required('myapp.can_delete', raise_exception=True)
+def delete_article(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        article.delete()
+        return redirect('article_list')
+    return render(request, 'myapp/delete_article.html', {'article': article})

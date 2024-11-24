75dace8bc2faa30e8bf77f28ceb0f2f0b404bda8
yomancode <yordiasfaw9@gmail.com>
2024-11-24 10:28:22 +0300

    permission

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.core.exceptions import PermissionDenied
 from .models import Book
 
+@permission_required('app_name.can_view', raise_exception=True)  # Replace 'app_name' with your app's name
 def book_list(request):
     """
-    View to display a list of all books.
+    View to display a list of all books, restricted by permission.
     """
-    books = Book.objects.all()  # Fetch all book records from the database
-    return render(request, 'books/book_list.html', {'books': books})
+    try:
+        books = Book.objects.all()  # Fetch all books
+        return render(request, 'books/book_list.html', {'books': books})
+    except PermissionDenied:
+        return render(request, '403.html', status=403)  # Render a custom "403 Forbidden" page

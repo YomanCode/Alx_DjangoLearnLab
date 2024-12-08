a1e65157ce1e1cf5622c39b0e303cfa73658bd0a
yomancode <yordiasfaw9@gmail.com>
2024-12-08 15:20:31 +0300

    tag

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Comment
-from .models import Post, Tag
+from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -18,8 +18,9 @@ class CommentForm(forms.ModelForm):
         fields = ['content']
 
 class PostForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  # Use the TagWidget for tags field
+        }

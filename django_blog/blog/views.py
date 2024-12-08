25c24f7f7b0ec100ed652a4649c6e0bd1be0b76c
yomancode <yordiasfaw9@gmail.com>
2024-12-08 12:27:01 +0300

    User profile

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile view
+def profile(request):
+    return render(request, 'blog/profile.html')

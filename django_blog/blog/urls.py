464610eb6543066eaffed43d6fe289ab1c3807a5
yomancode <yordiasfaw9@gmail.com>
2024-12-08 14:52:12 +0300

    comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit-comment'),
-    path('posts/<int:post_id>/comments/new/', views.add_comment, name='add-comment'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
+    path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name='edit-comment'),
+    path("post/<int:pk>/comments/new/", views.add_comment, name='add-comment'),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name='delete-comment'),
     path('', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
